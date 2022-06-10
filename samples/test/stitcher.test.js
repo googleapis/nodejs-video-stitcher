1a92296ad50133d1b7465e3ab198ea54e04efde9
Nick Cook <nickcook@google.com>
2022-06-10 14:32:09 -0700

    lint run

--- samples/test/stitcher.test.js
+++ samples/test/stitcher.test.js
@@ -23,7 +23,6 @@ const {describe, it, before, after} = require('mocha');
 
 const uniqueId = uuidv4().split('-')[0];
 const bucketName = 'cloud-samples-data/media';
-const vodFileName = 'hls-vod/manifest.m3u8';
 
 const projectId = process.env.GCLOUD_PROJECT;
 const location = 'us-central1';
@@ -40,12 +39,6 @@ const hostname = 'cdn.example.com';
 const gCdnKeyname = 'gcdn-test-key';
 const gCdnPrivateKey = 'VGhpcyBpcyBhIHRlc3Qgc3RyaW5nLg==';
 const akamaiTokenKey = 'VGhpcyBpcyBhIHRlc3Qgc3RyaW5nLg==';
-
-const vodUri = `https://storage.googleapis.com/${bucketName}/${vodFileName}`;
-// VMAP Pre-roll (https://developers.google.com/interactive-media-ads/docs/sdks/html5/client-side/tags)
-const adTagUri = 'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpreonly&ciu_szs=300x250%2C728x90&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&correlator=';
-const vodSessionName = `/locations/${location}/vodSessions/`;
-
 const cwd = path.join(__dirname, '..');
 
 before(async () => {
@@ -206,35 +199,4 @@ describe('CDN key functions', () => {
     );
     assert.ok(output.includes('Deleted CDN key'));
   });
-});
-
-describe('VOD session functions', () => {
-  it('should create a VOD session', () => {
-    const output = execSync(
-      `node createVodSession.js ${projectId} ${location} ${vodUri} ${adTagUri}`,
-      {cwd}
-    );
-    assert.ok(output.includes(vodSessionName));
-    this.vodSessionId = output.toString().split('/').pop();
-  });
-
-  it('should get a VOD session', () => {
-    const output = execSync(
-      `node getVodSession.js ${projectId} ${location} ${this.vodSessionId}`,
-      {cwd}
-    );
-    assert.ok(output.includes(vodSessionName));
-  });
-
-  // No list or delete methods for VOD sessions
-
-  // Ad tag details
-
-  // it('should delete a slate', () => {
-  //   const output = execSync(
-  //     `node deleteSlate.js ${projectId} ${location} ${slateId}`,
-  //     {cwd}
-  //   );
-  //   assert.ok(output.includes('Deleted slate'));
-  // });
 });
